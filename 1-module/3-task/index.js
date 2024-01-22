871a245f4f18371d16e55fe7026e65187ba40bd8
timosha <tinaatimkovaa@gmail.com>
2024-01-22 04:05:01 -0500

    first letter in the str to upper case

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,12 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length == 0) {
+    return str;
+  } else if (str.length == 1) {
+    return str.toUpperCase();
+  }
+
+  return str[0].toUpperCase() + str.slice(1, str.length); 
 }
+
+
+// slice(indexStart, indexEnd). indexEnd: the index of the first character to EXCLUDE from the returned substring.
